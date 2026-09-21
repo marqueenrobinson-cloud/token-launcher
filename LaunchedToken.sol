8895d74b09584be2a4bc3c02ba17d28ce8c8d397
marqueenrobinson-cloud <marqueenrobinson@gmail.com>
2026-09-21 13:12:38 -0500

    Implement LaunchedToken contract with ERC20 standard

--- LaunchedToken.sol
+++ LaunchedToken.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LaunchedToken is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 supply,
+        address creator
+    ) ERC20(name, symbol) {
+        _mint(creator, supply * 10 ** decimals());
+    }
+}
