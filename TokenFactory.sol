822e9277d307ee4ca0b48378856b6e86f53a476b
marqueenrobinson-cloud <marqueenrobinson@gmail.com>
2026-09-21 13:14:39 -0500

    Refactor TokenFactory by removing LaunchedToken
    Removed LaunchedToken contract definition and updated imports.

--- TokenFactory.sol
+++ TokenFactory.sol
@@ -1,18 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract LaunchedToken is ERC20 {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 supply,
-        address creator
-    ) ERC20(name, symbol) {
-        _mint(creator, supply * 10 ** decimals());
-    }
-}
+import "./LaunchedToken.sol";
 
 contract TokenFactory {
     address public owner;
